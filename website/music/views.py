27491817a9aefe22916c47bd304c3b7724a4d5b5
kurtgee6 <kurtgee11@gmail.com>
2017-10-04 11:12:39 -0400

    created a basic music app homepage

--- website/music/views.py
+++ website/music/views.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>" + "This is the music app homepage" + "</h1>")
