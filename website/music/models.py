4bfa999fd86b36064fb48fce597b1adfddf372f3
kurtgee6 <kurtgee11@gmail.com>
2017-10-04 11:41:42 -0400

    created classes

--- website/music/models.py
+++ website/music/models.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Album(models.model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1500)
+
+class Song(models.model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=30)
+    song_title = models.CharField(max_length=500)
