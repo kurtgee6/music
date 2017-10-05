9657943a668330e30a3b31c52a17261c817998bd
kurtgee6 <kurtgee11@gmail.com>
2017-10-05 12:53:13 -0400

    template shortcut

--- website/music/views.py
+++ website/music/views.py
@@ -2,19 +2,20 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     # connection to the database
     all_albums = Album.objects.all()
-    # template
-    template = loader.get_template('music/index.html')
+
     #dictionary
     context = {
         'all_albums': all_albums
     }
-    return HttpResponse(template.render(context, request))
+
+    #rendering html page
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>" )
