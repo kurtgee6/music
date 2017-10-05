37f9ce8e77cc6333fe60925fd118d4681de11abb
kurtgee6 <kurtgee11@gmail.com>
2017-10-05 14:14:11 -0400

    update detail.html and added songs to albums

--- website/music/models.py
+++ website/music/models.py
@@ -18,3 +18,6 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=30)
     song_title = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.song_title
