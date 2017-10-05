1e6389652f7e3b14cc3e2f5094dc0afd69f53236
kurtgee6 <kurtgee11@gmail.com>
2017-10-05 12:44:00 -0400

    added new template

--- website/music/views.py
+++ website/music/views.py
@@ -2,19 +2,19 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album
 
 def index(request):
     # connection to the database
     all_albums = Album.objects.all()
-    html = ''
-    
-    # for loop to grab each objects in Album
-    for album in all_albums:
-        
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="'+ url +'">'+ album.album_title +'</a><br>'
-    return HttpResponse(html)
+    # template
+    template = loader.get_template('music/index.html')
+    #dictionary
+    context = {
+        'all_albums': all_albums
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>" )
