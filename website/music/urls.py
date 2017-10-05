0166ff43810eba0726a37b42a295724832c04620
kurtgee6 <kurtgee11@gmail.com>
2017-10-05 12:07:23 -0400

    updating music application

--- website/music/urls.py
+++ website/music/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     # main page 
     url(r'^$', views.index, name='index'),
+
+    # /music/someID
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
 ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from .models import Album
 
 def index(request):
-    return HttpResponse("<h1>" + "This is the music app homepage" + "</h1>")
+    # connection to the database
+    all_albums = Album.objects.all()
+    html = ''
+    
+    # for loop to grab each objects in Album
+    for album in all_albums:
+        
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href="'+ url +'">'+ album.album_title +'</a><br>'
+    return HttpResponse(html)
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>" )
